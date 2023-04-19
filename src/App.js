f6277f1e376a7e1bdacff03cf1834c6094a0c8e8
NeduBullion <88090469+NeduBullion@users.noreply.github.com>
2023-04-19 06:41:05 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Notes from "./components/Notes";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+const react = require("react");
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Notes.jsx
@@ -0,0 +1,15 @@
+const react = require("react");
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>React.js Bootcamp</h1>
+      <p>
+        We learn the basics of wed development, Javascript, Jsx, React, ReactDom
+        and created a project at the very end of the bootcamp as well.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
